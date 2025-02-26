e244b3d0872443cba39c675e25c08a92511e2969
timchristoffer <timdaahl@gmail.com>
2025-02-26 16:48:52 +0100

    Bugged Migrations

--- kit-backend/Migrations/20250226154731_InitialCreate.Designer.cs
+++ kit-backend/Migrations/20250226154731_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace kit_backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250226150204_InitialCreate")]
+    [Migration("20250226154731_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
