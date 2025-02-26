5e215a7b7eb5dce79ba5318c3680ebb7eebe8986
timchristoffer <timdaahl@gmail.com>
2025-02-26 13:48:41 +0100

    Another try to fix install_dotnet.sh

--- kit-backend/install_dotnet.sh
+++ kit-backend/install_dotnet.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-curl -sSL https://download.visualstudio.microsoft.com/download/pr/7a2e3f36-47e9-4638-bcb3-d598a0758a3e/9fc40566d72b5ac4e1d38f9704f17398/dotnet-sdk-7.0.100-linux-x64.tar.gz | tar -xvzf - -C /root/.dotnet
-export PATH=$PATH:/root/.dotnet
+# Installera .NET SDK via apt
+apt-get update
+apt-get install -y wget apt-transport-https software-properties-common
+wget https://packages.microsoft.com/config/ubuntu/20.04/prod.list
+mv prod.list /etc/apt/sources.list.d/dotnetdev.list
+apt-get update
+apt-get install -y dotnet-sdk-7.0
