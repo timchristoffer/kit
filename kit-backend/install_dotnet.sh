7fbd6853addc1684c0a879240b6cd6a45a899d41
timchristoffer <timdaahl@gmail.com>
2025-02-26 13:53:38 +0100

    Another fix for insta_dotnet.sh

--- kit-backend/install_dotnet.sh
+++ kit-backend/install_dotnet.sh
@@ -3,9 +3,22 @@
 
 # Definiera versionen du vill installera
 DOTNET_VERSION="7.0.100"
+DOTNET_INSTALL_DIR="/root/.dotnet"
 
-# Ladda ner .NET SDK i en lokal katalog
-curl -sSL https://dotnet.microsoft.com/download/dotnet/thank-you/dotnet-sdk-${DOTNET_VERSION}-linux-x64-binaries | tar -xz -C /root/.dotnet
+# Skapa katalogen om den inte finns
+mkdir -p $DOTNET_INSTALL_DIR
+
+# Ladda ner den rätta .NET SDK-versionen
+curl -sSL https://download.visualstudio.microsoft.com/download/pr/0b321f26-55a2-4502-b118-b6c77e4f45d1/a3eb82cfe38279556a88ee745e8de99b/dotnet-sdk-${DOTNET_VERSION}-linux-x64.tar.gz -o dotnet-sdk.tar.gz
+
+# Extrahera SDK till rätt katalog
+tar -xzf dotnet-sdk.tar.gz -C $DOTNET_INSTALL_DIR
+
+# Ta bort den nedladdade filen
+rm dotnet-sdk.tar.gz
 
 # Lägg till .NET SDK i PATH så det kan användas
-export PATH=$PATH:/root/.dotnet
+export PATH=$PATH:$DOTNET_INSTALL_DIR
+
+# Kontrollera om .NET SDK är korrekt installerad
+dotnet --version
